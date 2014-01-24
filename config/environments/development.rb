2d6c81e294696745b22e816cfd9bf7bcb7514839
alinusgallus <gall.alain@gmail.com>
2014-01-24 10:33:35 +0800

    add configuration for email, Mandril and Mailchimp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,13 @@ LearnRails::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  #copy/paste from learn-rails for email configuration
+  config.action_mailer.smtp_settings = {
+  :address   => "smtp.mandrillapp.com",
+  :port      => 25,
+  :user_name => ENV["MANDRILL_USERNAME"],
+  :password  => ENV["MANDRILL_API_KEY"]
+}
+
 end
